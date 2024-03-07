4d64a27f99e9190e5292b518f75508bff047a866
Peter Teuben <teuben@gmail.com>
2024-03-07 08:32:03 -0500

    two new

--- mk_runs.py
+++ mk_runs.py
@@ -10,7 +10,7 @@ project="2024-S1-MX-2"
 
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
-on["G358"] = [ 112139,]
+on["G358"] = [ 112139,112292, 112296,]
 
 
 #        common parameters per source on the first dryrun (run1a, run2a)
