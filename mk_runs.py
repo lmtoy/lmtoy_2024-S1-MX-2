354f412b42d9a161320c978891254b6262d0a751
Peter Teuben <teuben@gmail.com>
2024-03-10 09:02:57 -0400

    try blanking all 3 beams before resetting them per bank

--- mk_runs.py
+++ mk_runs.py
@@ -15,11 +15,11 @@ on["G358"] = [ 112139, 112292, 112296,]
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
-pars1['G358']   = "pix_list=-13 extent=500 birdies=1021,1024,1027,1637,2045,2048,2051"
+pars1['G358']   = "pix_list=-13,14,15 extent=500 birdies=1021,1024,1027,1637,2045,2048,2051"
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
-pars2['G358']   = "bank=0"
+pars2['G358']   = "bank=0 pix_list=-13"
 
 pars3 = {}
 pars3['G358']   = "bank=1 pix_list=-13,14,15"
