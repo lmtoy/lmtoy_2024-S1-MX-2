f27164772b9acba5b94c7e58dfd6510750f0c424
Peter Teuben <teuben@gmail.com>
2024-11-27 14:55:37 +0000

    new data

--- mk_runs.py
+++ mk_runs.py
@@ -11,16 +11,17 @@ project="2024-S1-MX-2"
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
 
-on["G1.7+3.7-234_01_of_04"]   =  [ 117345,  120317, 120409, ]
-on["G1.7+3.7-234_02_of_04"]   =  [ 117349,  120319,]
-on["G1.7+3.7-234_03_of_04"]   =  [ 112524,  120323,]
-on["G1.7+3.7-234_04_of_04"]   =  [ 112532,  120325, 120407, ]
+on["G1.7+3.7-234_01_of_04"]   =  [ 117345,  120317, 120409, 110041,]
+on["G1.7+3.7-234_02_of_04"]   =  [ 117349,  120319, 120516, 110049,]
+on["G1.7+3.7-234_03_of_04"]   =  [ 112524,  120323, 120518, 110053,]
+on["G1.7+3.7-234_04_of_04"]   =  [ 112532,  120325, 120407, 120651,]
 
 
 # 110053  110049  110041  are also the G1 source, in 2023-S1-UM-16
+# MH: 08/13/2024 -- added 2023 to 2024 data
 
 
-on["G358.7+3.7+179_01_of_04"] =  [ 112139, 112292, 112296, 120057, 120061, 120214,]
+on["G358.7+3.7+179_01_of_04"] =  [ -112139, -112292, -112296, 120057, 120061, 120214,]
 on["G358.7+3.7+179_02_of_04"] =  [ 112518, 112788, 117039,]
 on["G358.7+3.7+179_03_of_04"] =  [ 113027, 113137, 117043,]
 on["G358.7+3.7+179_04_of_04"] =  [ 113029, 113139, 117047,]
@@ -33,41 +34,42 @@ on["G358-mos"] = on["G358.7+3.7+179_01_of_04"] + on["G358.7+3.7+179_02_of_04"] +
  
 
 #        common parameters per source on the first dryrun (run1a, run2a)
+#Added otf_cal=1
 pars1 = {}
-pars1["G1.7+3.7-234_01_of_04"]   = "pix_list=-15 extent=500 b_order=3 dv=30 dw=30 vlsr=-234"
-pars1["G1.7+3.7-234_02_of_04"]   = "pix_list=-15 extent=500 b_order=3 dv=30 dw=30 vlsr=-234"
-pars1["G1.7+3.7-234_03_of_04"]   = "pix_list=-15 extent=500 b_order=3 dv=30 dw=30 vlsr=-234"
-pars1["G1.7+3.7-234_04_of_04"]   = "pix_list=-15 extent=500 b_order=3 dv=30 dw=30 vlsr=-234"
+pars1["G1.7+3.7-234_01_of_04"]   = "pix_list=-15 extent=500 b_order=1 dv=50 dw=100 vlsr=-234 otf_cal=1"
+pars1["G1.7+3.7-234_02_of_04"]   = "pix_list=-15 extent=500 b_order=1 dv=50 dw=100 vlsr=-234 otf_cal=1"
+pars1["G1.7+3.7-234_03_of_04"]   = "pix_list=-15 extent=500 b_order=1 dv=50 dw=100 vlsr=-234 otf_cal=1"
+pars1["G1.7+3.7-234_04_of_04"]   = "pix_list=-15 extent=500 b_order=1 dv=50 dw=100 vlsr=-234 otf_cal=1"
 pars1["G1-mos"]                  = "skip=1"     # comment out if no mosaic desired
 
 # M Heyer  04/17./2024 -- vlsr =179 and pix_list=-13  (not 13,14,15)  dv=20 dw=50
-pars1["G358.7+3.7+179_01_of_04"] = "pix_list=-15 extent=500 b_order=1 dv=20 dw=50 vlsr=179"
-pars1["G358.7+3.7+179_02_of_04"] = "pix_list=-15 extent=500 b_order=1 dv=20 dw=50 vlsr=179"
-pars1["G358.7+3.7+179_03_of_04"] = "pix_list=-15 extent=500 b_order=1 dv=20 dw=50 vlsr=179"
-pars1["G358.7+3.7+179_04_of_04"] = "pix_list=-15 extent=500 b_order=1 dv=20 dw=50 vlsr=179"
+pars1["G358.7+3.7+179_01_of_04"] = "pix_list=-15 extent=500 b_order=1 dv=50 dw=100 vlsr=179 otf_cal=1"
+pars1["G358.7+3.7+179_02_of_04"] = "pix_list=-15 extent=500 b_order=1 dv=50 dw=100 vlsr=179 otf_cal=1"
+pars1["G358.7+3.7+179_03_of_04"] = "pix_list=-15 extent=500 b_order=1 dv=50 dw=100 vlsr=179 otf_cal=1"
+pars1["G358.7+3.7+179_04_of_04"] = "pix_list=-15 extent=500 b_order=1 dv=50 dw=100 vlsr=179 otf_cal=1"
 pars1["G358-mos"]                = "skip=1"     # comment out if no mosaic desired
 
 #        common parameters per source on subsequent runs (run1b, run2b)
 pars2 = {}
-pars2["G1.7+3.7-234_01_of_04"]   = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13"
-pars2["G1.7+3.7-234_02_of_04"]   = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13"
-pars2["G1.7+3.7-234_03_of_04"]   = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13"
-pars2["G1.7+3.7-234_04_of_04"]   = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13"
-pars2["G358.7+3.7+179_01_of_04"] = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13"
-pars2["G358.7+3.7+179_02_of_04"] = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13"
-pars2["G358.7+3.7+179_03_of_04"] = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13"
-pars2["G358.7+3.7+179_04_of_04"] = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13"
+pars2["G1.7+3.7-234_01_of_04"]   = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13 otf_cal=1"
+pars2["G1.7+3.7-234_02_of_04"]   = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13 otf_cal=1"
+pars2["G1.7+3.7-234_03_of_04"]   = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13 otf_cal=1"
+pars2["G1.7+3.7-234_04_of_04"]   = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13 otf_cal=1"
+pars2["G358.7+3.7+179_01_of_04"] = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13 otf_cal=1"
+pars2["G358.7+3.7+179_02_of_04"] = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13 otf_cal=1"
+pars2["G358.7+3.7+179_03_of_04"] = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13 otf_cal=1"
+pars2["G358.7+3.7+179_04_of_04"] = "bank=0 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13 otf_cal=1"
 
 
 pars3 = {}
-pars3["G1.7+3.7-234_01_of_04"]   = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15"
-pars3["G1.7+3.7-234_02_of_04"]   = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15"
-pars3["G1.7+3.7-234_03_of_04"]   = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15"
-pars3["G1.7+3.7-234_04_of_04"]   = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15"
-pars3["G358.7+3.7+179_01_of_04"] = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15"
-pars3["G358.7+3.7+179_02_of_04"] = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15"
-pars3["G358.7+3.7+179_03_of_04"] = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15"
-pars3["G358.7+3.7+179_04_of_04"] = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15"
+pars3["G1.7+3.7-234_01_of_04"]   = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15 otf_cal=1"
+pars3["G1.7+3.7-234_02_of_04"]   = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15 otf_cal=1"
+pars3["G1.7+3.7-234_03_of_04"]   = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15 otf_cal=1"
+pars3["G1.7+3.7-234_04_of_04"]   = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15 otf_cal=1"
+pars3["G358.7+3.7+179_01_of_04"] = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15 otf_cal=1"
+pars3["G358.7+3.7+179_02_of_04"] = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15 otf_cal=1"
+pars3["G358.7+3.7+179_03_of_04"] = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15 otf_cal=1"
+pars3["G358.7+3.7+179_04_of_04"] = "bank=1 birdies=1021,1024,1027,1637,2045,2048,2051 pix_list=-13,14,15 otf_cal=1"
 
 
 
